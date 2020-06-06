bec4b0ab66fdda516a1a3351629aac5edb0389a8
med-amine-fh <fhal.mohamed.amine@gmail.com>
2020-06-06 14:06:06 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const btn = document.querySelectorAll("[data-time]");
+
+let countDown;
+const timer = (seconds) => {
+  clearInterval(countDown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+  countDown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countDown);
+      timerDisplay.textContent = "--:--";
+      timerDisplay.style.color = "#122";
+      endTime.textContent = "Ended!!";
+      document.title = "Done";
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+};
+
+const displayTimeLeft = (s) => {
+  const min = Math.floor(s / 60);
+  const remainder = s % 60;
+  const display = `${min}:${remainder < 10 ? "0" : ""}${remainder}`;
+  document.title = display;
+  min === 0 && remainder <= 15
+    ? (timerDisplay.style.color = "#ff0129")
+    : (timerDisplay.style.color = "#111");
+  timerDisplay.textContent = display;
+};
+
+const displayEndTime = (a) => {
+  const end = new Date(a);
+  const hour = end.getHours();
+  const adjustHour = hour > 12 ? hour - 12 : hour;
+  const min = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustHour}:${
+    min < 10 ? "0" : ""
+  }${min} ${hour > 12 ? "PM" : "AM"}`;
+};
+
+btn.forEach((a) => {
+  a.addEventListener("click", function () {
+    const sec = Number(this.dataset.time);
+    timer(sec);
+  });
+});
+
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const min = this.minutes.value;
+  console.log(min);
+  timer(min * 60);
+  this.reset();
+});
